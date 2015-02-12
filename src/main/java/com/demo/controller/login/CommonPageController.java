219514e3d955a5b64c95f439378ce5f86cf1d0dd
Charnjeet Singh <charnjeet.iotasol@gmail.com>
2015-02-12 17:01:05 +0530

    Redirect Page

--- src/main/java/com/demo/controller/login/CommonPageController.java
+++ src/main/java/com/demo/controller/login/CommonPageController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.demo.service.security.SecurityInfoProvider;
 
@@ -30,14 +29,11 @@ public class CommonPageController {
 	 * @return ModelAndView
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView welcome() {
+	public String welcome() {
 
-		ModelAndView model = new ModelAndView();
 		if (securityInfoProvider.getCurrentUser() != null) {
-			model.setViewName("userHome");
-			return model;
+			return "redirect:user";
 		}
-		model.setViewName("welcome");
-		return model;
+		return "welcome";
 	}
 }
